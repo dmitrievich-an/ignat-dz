09d9faf5765024fc02ba55a942576a2053ac0318
Artem <reebok001@yandex.ru>
2023-01-25 00:10:53 +0300

    finished the fourth task

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,10 +1,10 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
-import { UserType } from './HW3'
+import {UserType} from './HW3'
 
 type GreetingContainerPropsType = {
-    users: UserType[] // need to fix any
-    addUserCallback: (name: string) => void // need to fix any
+  users: UserType[] // need to fix any
+  addUserCallback: (name: string) => void // need to fix any
 }
 
 export const pureAddUser = (name: string, setError: (error: string) => void, setName: (name: string) => void, addUserCallback: (name: string) => void) => {
@@ -15,7 +15,7 @@ export const pureAddUser = (name: string, setError: (error: string) => void, set
     setName('')
     setError('')
   }
-    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+  // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
 }
 
 export const pureOnBlur = (name: string, setError: (error: string) => void) => { // если имя пустое - показать ошибку
@@ -31,45 +31,45 @@ export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: () => v
 
 // более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
-    // деструктуризация пропсов
-    const [name, setName] = useState<string>('') // need to fix any
-    const [error, setError] = useState<string>('') // need to fix any
+                                                                   users,
+                                                                   addUserCallback,
+                                                                 }) => {
+  // деструктуризация пропсов
+  const [name, setName] = useState<string>('') // need to fix any
+  const [error, setError] = useState<string>('') // need to fix any
 
-    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
-        setName(e.currentTarget.value) // need to fix
+  const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+    setName(e.currentTarget.value) // need to fix
 
-        error && setError('')
-    }
-    const addUser = () => {
-        pureAddUser(name, setError, setName, addUserCallback)
-    }
+    error && setError('')
+  }
+  const addUser = () => {
+    pureAddUser(name, setError, setName, addUserCallback)
+  }
 
-    const onBlur = () => {
-        pureOnBlur(name, setError)
-    }
+  const onBlur = () => {
+    pureOnBlur(name, setError)
+  }
 
-    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-        pureOnEnter(e, addUser)
-    }
+  const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+    pureOnEnter(e, addUser)
+  }
 
-    const totalUsers = users.length // need to fix
-    const lastUserName = totalUsers > 0 ? users[users.length - 1].name : undefined // need to fix
+  const totalUsers = users.length // need to fix
+  const lastUserName = totalUsers > 0 ? users[users.length - 1].name : undefined // need to fix
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            onBlur={onBlur}
-            onEnter={onEnter}
-            error={error}
-            totalUsers={totalUsers}
-            lastUserName={lastUserName}
-        />
-    )
+  return (
+    <Greeting
+      name={name}
+      setNameCallback={setNameCallback}
+      addUser={addUser}
+      onBlur={onBlur}
+      onEnter={onEnter}
+      error={error}
+      totalUsers={totalUsers}
+      lastUserName={lastUserName}
+    />
+  )
 }
 
 export default GreetingContainer
--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,8 +27,9 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // задачка на написание онченджа
-
+      if (onChangeChecked) {
+        onChangeChecked(!restProps.checked)
+      }
     }
 
     const finalInputClassName = s.checkbox
